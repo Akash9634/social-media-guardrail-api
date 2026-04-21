3aa8ef605d487124566ddf65b4cca44423277948
Akash9634 <akashkush2020@gmail.com>
2026-04-22 00:12:53 +0530

    initial commit

---
+++ src/main/java/com/media/socialgaurd/SocialgaurdApplication.java
@@ -0,0 +1,13 @@
+package com.media.socialgaurd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialgaurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialgaurdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/media/socialgaurd/SocialgaurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.media.socialgaurd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialgaurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
