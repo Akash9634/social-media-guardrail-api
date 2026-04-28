298b56a2dfc8b756bb88a9cdfd0af37431eb566d
Akash9634 <akashkush2020@gmail.com>
2026-04-28 12:30:56 +0530

    DTOs and JwtService implemented

---
+++ src/main/java/com/media/socialgaurd/DTO/RegisterRequest.java
@@ -0,0 +1,34 @@
+package com.media.socialgaurd.DTO;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    // @NotBlank = cannot be null or empty string
+    @NotBlank(message = "Username is required")
+    // @Size = minimum 3, maximum 20 characters
+    @Size(min = 3, max = 20, message = "Username must be 3-20 characters")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+    // @Email = must be valid email format
+    @NotBlank(message = "Email is required")
+    @Email(message = "Must be a valid email")
+    private String email;
+
+    // Optional field - defaults to false if not provided
+    private Boolean isPremium = false;
+}
---
+++ src/main/java/com/media/socialgaurd/Security/JwtService.java
@@ -0,0 +1,42 @@
+package com.media.socialgaurd.Security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
+
+    //generate a token
+    public String generateToken(String username){
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+    private Claims extractClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigningKey(){
+
+    }
+
+}
